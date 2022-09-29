5d47e1ea4dabe0783233c57034328f5e0d88c4d9
SJHA <sjha@diquest.com>
2022-09-29 14:54:30 +0900

    test

---
+++ src/main/java/com/diquest/compare/CompareBugsApplication.java
@@ -0,0 +1,9 @@
+package com.diquest.compare;
+
+import org.springframework.boot.SpringApplication;
+
+public class CompareBugsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CompareBugsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/CompareBugsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareBugsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareBugsApplication.class, args);
+	}
+
+}
